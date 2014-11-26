161a782bd5e1dc0019e1040905f9ba02a45b6053
Ghislain Proulx <gproulx@ghislainproulx.net>
2014-11-25 20:41:36 -0500

    Filter: Refactoring (JS bundle)

---
+++ Website/Content/js/general.js
@@ -0,0 +1,24 @@
+﻿$(function () {
+    $(".footer a, .kristensen").tooltip();
+    $('.navbar-custom .navbar-nav > li >  a[href*="' + window.location.pathname + '"]').parent().addClass("active");
+    $('.navbar-custom .dropdown-menu > li a[href*="' + window.location.pathname + '"]').parent().addClass("active");
+    $('.navbar-custom .dropdown-menu > li a[href*="' + window.location.pathname + '"]').parent().parent().parent().addClass("active");
+});
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1);
+        if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
+    }
+    return "";
+}
